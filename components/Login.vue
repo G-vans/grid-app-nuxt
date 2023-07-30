cf4f7730c2483ad42e6e157d78fffebaa32898a0
Jevans <jevansstanley@gmail.com>
2023-07-31 00:26:15 +0300

    feat: implemet login authentication

---
+++ components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="login">
+      <h2>Login</h2>
+      <form @submit.prevent="login">
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" id="username" v-model="username" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: "",
+        password: "",
+      };
+    },
+    methods: {
+      login() {
+        const hardcodedUsername = "demo";
+        const hardcodedPassword = "qwerty123";
+  
+        // Check if the entered credentials match the credentials
+        if (this.username === hardcodedUsername && this.password === hardcodedPassword) {
+          // If login is successful, emit an event to notify the parent component
+          this.$emit("login-success");
+        } else {
+          // If login fails, show an error message (for simplicity, we'll just log an error)
+          console.error("Invalid credentials. Please try again.");
+        }
+      },
+    },
+  };
+  </script>
+  
+  
